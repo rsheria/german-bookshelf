8e65cfd31bb4dcb88240cd99a584d9618fe5638c
rsheria <rsheria2009@gmail.com>
2025-04-02 20:52:58 +0200

    Fix React application setup with proper providers and routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
+import './i18n/i18n'
+import { AuthProvider } from './context/AuthContext'
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ChakraProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
